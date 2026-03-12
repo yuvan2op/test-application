3f622f639b4434219d9c4e16fab2758cf4a6ffd7
yuvan2op <yuvan2op@gmail.com>
2026-03-12 09:41:45 +0000

    k8s

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Backend 🚀" });
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on port 3000");
+});
