b46dbc90e9e254b2a07b92f5876c5c9ae58389f1
yuvan2op <yuvan2op@gmail.com>
2026-03-20 05:10:53 +0000

    helm

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from Backend 🚀" });
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(3000, () => {
   console.log("Backend running on port 3000");
 });
