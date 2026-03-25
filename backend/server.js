0a863f935b619acf06031ec0735e773206012adf
yuvan2op <yuvan2op@gmail.com>
2026-03-25 06:32:41 +0000

    no localhost

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from Backend 🚀" });
 });
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Backend running on port 3000");
 });
