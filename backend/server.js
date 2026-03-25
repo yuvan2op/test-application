0b5c526a5f14f57eec922bd08f9339ea484b4f20
yuvan2op <yuvan2op@gmail.com>
2026-03-25 06:32:41 +0000

    working on nodeip

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from Backend 🚀" });
 });
 
+<<<<<<< HEAD
 app.listen(3000, () => {
+=======
+app.get("/api/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+>>>>>>> 0a863f9 (no localhost)
   console.log("Backend running on port 3000");
 });
