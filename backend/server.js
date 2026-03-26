bf7d9b76132ad95d2929ff593d5124e6e2dc79f7
yuvan2op <yuvan2op@gmail.com>
2026-03-26 10:45:18 +0000

    workin

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Backend running on port 3000");
 });
+
